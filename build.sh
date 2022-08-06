197ebe2b34cd9ccacfcfb20aaf8c55b65cc5f675
Brian Li <brianli1117@gmail.com>
2022-08-06 16:20:09 -0400

    update build, storage

--- build.sh
+++ build.sh
@@ -1,36 +1,38 @@
+#!/bin/zsh
 
-if [ -f "./marketplace_common/marketplace_common.pb.go" ]
-then
-    rm ./marketplace_common/marketplace_common.pb.go
-fi
-protoc -I=. --go_out=./marketplace_common ./marketplace_common/marketplace_common.proto
-mv ./marketplace_common/github.com/blidd/fractr-proto/marketplace_common/marketplace_common.pb.go ./marketplace_common
-rm -r ./marketplace_common/github.com
+for dir in ./*/
+do
+    PROTO="${dir%/}/$(basename "$dir").proto"
+    PB="${dir%/}/$(basename "$dir").pb.go"
+    GRPC="${dir%/}/$(basename "$dir")_grpc.pb.go"
 
-if [ -f "./marketplace_secondary/marketplace_secondary.pb.go" ]
-then
-    rm ./marketplace_secondary/marketplace_secondary.pb.go
-fi
-if [ -f "./marketplace_secondary/marketplace_secondary_grpc.pb.go" ]
-then
-    rm ./marketplace_secondary/marketplace_secondary_grpc.pb.go
-fi
-protoc -I=. --go_out=./marketplace_secondary --go-grpc_out=./marketplace_secondary ./marketplace_secondary/marketplace_secondary.proto
-mv ./marketplace_secondary/github.com/blidd/fractr-proto/marketplace_secondary/marketplace_secondary.pb.go ./marketplace_secondary
-mv ./marketplace_secondary/github.com/blidd/fractr-proto/marketplace_secondary/marketplace_secondary_grpc.pb.go ./marketplace_secondary
-rm -r ./marketplace_secondary/github.com
+    echo "generating gRPC files for ${PROTO}..."
+    
+    if [ -f ${PB} ]
+    then
+        rm ${PB}
+    fi
 
-if [ -f "./service/service.pb.go" ]
-then
-    rm ./service/service.pb.go
-fi
+    if [ -f ${GRPC} ]
+    then
+        rm ${GRPC}
+    fi
 
-if [ -f "./service/service_grpc.pb.go" ]
-then
-    rm ./service/service_grpc.pb.go
-fi
+    protoc -I=. --go_out=$dir --go-grpc_out=$dir ${PROTO}
 
-protoc -I=. --go_out=./service --go-grpc_out=./service ./service/service.proto
-mv ./service/github.com/blidd/fractr-proto/service/service.pb.go ./service
-mv ./service/github.com/blidd/fractr-proto/service/service_grpc.pb.go ./service
-rm -r ./service/github.com
+    GEN_PB="${dir%/}/github.com/blidd/fractr-proto/$(basename $dir)/$(basename $dir).pb.go"
+    GEN_GRPC="${dir%/}/github.com/blidd/fractr-proto/$(basename $dir)/$(basename $dir)_grpc.pb.go"
+
+    if [ -f ${GEN_PB} ]
+    then
+        mv ${GEN_PB} $dir
+    fi
+    
+    if [ -f ${GEN_GRPC} ]
+    then
+        mv ${GEN_GRPC} $dir
+    fi
+
+    rm -r ${dir%/}/github.com
+    
+done
